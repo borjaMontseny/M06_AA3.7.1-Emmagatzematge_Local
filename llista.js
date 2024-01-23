3dda2ac2df861aadd8a90355b2417a9bb203f204
Borja Montseny <borja.montseny@gmail.com>
2024-01-23 13:31:20 +0100

    Commit arrayProductes2
    Ara falta inserir els elements html segons local storage

--- llista.js
+++ llista.js
@@ -3,14 +3,20 @@ var quantitat;
 var marca;
 var botoAfegir;
 var arrayProductes = [];
+var arrayProductes2 = [];
 
 function recollirDades() {
     // En aquest array guardarem les 3 dades de cada producte
-    var arrayProducto = [];
-    // Afegim les dades del producte al array de producte
-    arrayProducto.push(producte.value, quantitat.value, marca.value);
-    // Afegim aquest arrayProducte al array de Productes
-    arrayProductes.push(arrayProducto);
+    var arrayProducte = [];
+
+    // Afegim les dades del producte a l'array de producte
+    arrayProducte.push(producte.value, quantitat.value, marca.value);
+
+    // Afegim aquest arrayProducte a l'array de Productes
+    arrayProductes.push(arrayProducte);
+
+    // Reescribim literalment el arrayProductes de LocalStorage cada cop
+    localStorage.setItem('arrayProductes', JSON.stringify(arrayProductes));
 }
 
 // assignem les variables als elements HTML
@@ -21,6 +27,19 @@ function init() {
     botoAfegir = document.getElementsByClassName("w3-input w3-button w3-border w3-teal")[0];
     // Creem el listener per recollirDades i l'assignem al botó
     botoAfegir.addEventListener("click", recollirDades, false);
+}
+
+function imprimirProductes() {
+
+    // Recuperem arrayProductes de LocalStorage
+    arrayProductes2 = JSON.parse(localStorage.getItem("arrayProductes"));
+
+    for (let index = 0; index < arrayProductes2.length; index++) {
+
+
+
+    }
+
 }
 
 window.addEventListener("load", init, false); 
